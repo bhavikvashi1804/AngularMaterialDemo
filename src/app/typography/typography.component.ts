47efc9f7bea1a177130515303527622d43034b85
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 12:39:51 +0530

    typograpgy: mat-display-4 to 1

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
-  styleUrls: ['./typography.component.css']
 })
 export class TypographyComponent implements OnInit {
 
