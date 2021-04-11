efb34b6e6ebd0885409dff2e041137fc3d49bd66
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 12:34:57 +0530

    create a new component for typography

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TypographyComponent } from './typography/typography.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'typo-graphy', component: TypographyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './typography/typography.component';
 //refer this link for imports: https://stackoverflow.com/questions/58594311/angular-material-index-d-ts-is-not-a-module
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TypographyComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
