5ac4313a1197605b1c2763c8e7e60092fa914567
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 15:54:36 +0530

    import MatBadgeModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -15,6 +16,7 @@ const MaterialComponents = [
   MatProgressSpinnerModule,
   MatToolbarModule,
   MatSidenavModule,
+  MatBadgeModule,
 ];
 
 @NgModule({
