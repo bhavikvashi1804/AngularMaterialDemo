c321dc4dd8c339163b683194cb86873dc5ade3be
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 14:27:12 +0530

    some changes

--- src/app/m-icons/m-icons.component.ts
+++ src/app/m-icons/m-icons.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-m-icons',
-  templateUrl: './m-icons.component.html',
+  templateUrl: './m-icons.component.html'
 })
 export class MIconsComponent implements OnInit {
 
