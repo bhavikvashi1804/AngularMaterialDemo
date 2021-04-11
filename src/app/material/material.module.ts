723d6dc052db1809535e241ed5ccfa3f4092e6df
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 13:37:31 +0530

    buttons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 const MaterialComponents = [MatButtonModule];
 
 @NgModule({
-  imports: [MaterialComponents],
-  exports: [MaterialComponents],
+  imports: [MaterialComponents, MatButtonToggleModule],
+  exports: [MaterialComponents, MatButtonToggleModule],
 })
 export class MaterialModule {}
