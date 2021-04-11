a25e39c628488364c5b7344e8c69045eda8ff3c7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-11 15:24:08 +0530

    nav bar done

--- src/app/nav-demo/nav-demo.component.ts
+++ src/app/nav-demo/nav-demo.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-demo',
   templateUrl: './nav-demo.component.html',
+  styleUrls: ['./myStyle.css'],
 })
 export class NavDemoComponent implements OnInit {
   constructor() {}
