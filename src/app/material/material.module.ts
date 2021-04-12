1f26d3b58300717ed0b612746f62fe0e9b43f0e6
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-12 09:51:10 +0530

    import  MatGridListModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -22,6 +23,7 @@ const MaterialComponents = [
   MatMenuModule,
   MatListModule,
   MatDividerModule,
+  MatGridListModule,
 ];
 
 @NgModule({
