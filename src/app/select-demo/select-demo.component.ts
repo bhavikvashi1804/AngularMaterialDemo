877842edbcfea44651c90073a0f876bf49986be1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-12 14:13:37 +0530

    Select demo done

--- src/app/select-demo/select-demo.component.ts
+++ src/app/select-demo/select-demo.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-select-demo',
   templateUrl: './select-demo.component.html',
-  styleUrls: ['./select-demo.component.css']
+  styleUrls: ['./select-demo.component.css'],
 })
 export class SelectDemoComponent implements OnInit {
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 
-  constructor() { }
+  selectedValue: string;
+  selectedValue1: string;
+  selectedValue2: string;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
