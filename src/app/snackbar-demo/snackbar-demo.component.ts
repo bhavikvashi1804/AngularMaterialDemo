843403cb5a740416f0c8b8b7276f446135c5cadd
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-12 15:48:35 +0530

    snackbar done

--- src/app/snackbar-demo/snackbar-demo.component.ts
+++ src/app/snackbar-demo/snackbar-demo.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar-demo',
   templateUrl: './snackbar-demo.component.html',
-  styleUrls: ['./snackbar-demo.component.css']
+  styleUrls: ['./snackbar-demo.component.css'],
 })
 export class SnackbarDemoComponent implements OnInit {
+  constructor(private _snackBar: MatSnackBar) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
-
 }
