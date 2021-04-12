3806ac5cacdd7c32cdda50689770a206271987f7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-12 15:26:14 +0530

    date picker done

--- src/app/datepick-demo/datepick-demo.component.ts
+++ src/app/datepick-demo/datepick-demo.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-datepick-demo',
   templateUrl: './datepick-demo.component.html',
-  styleUrls: ['./datepick-demo.component.css']
+  styleUrls: ['./datepick-demo.component.css'],
 })
 export class DatepickDemoComponent implements OnInit {
+  minDate = new Date();
+  maxDate = new Date(2022, 10, 10);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  dateFilter = (date) => {
+    const day = date.getDay();
+    return day != 0 && day != 6;
+  };
 }
