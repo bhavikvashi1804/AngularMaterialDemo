884d7ec49a1f561118f9d2575603cc83edbe5204
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-14 04:13:12 +0530

    create a list of sub components and apply the style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
